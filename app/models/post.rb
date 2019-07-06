f8e74462a76325c30e360f3c22cd7a23d0f29905
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-06 01:41:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
+class ClickbaitValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+      record.errors[attribute] << ("Not a clickbait title")
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
@@ -6,11 +14,5 @@ class Post < ActiveRecord::Base
 
 end
 
-class ClickbaitValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
-      record.errors[attribute] << ("Not a clickbait title")
-    end
-  end
-end
+
 
