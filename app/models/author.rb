2e367d44c930427af7531c58b861f3e6ac5d17bf
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-06 01:12:47 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,5 @@
 class Author < ActiveRecord::Base
-    validates :name, presence: true
-    validates :name, uniqueness: true
+    validates :name, presence: true, uniqueness: true
     validates :phone_number, length: { minimum: 10 }
-
 
 end
