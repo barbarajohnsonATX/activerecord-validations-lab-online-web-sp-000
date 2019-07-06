68f45ca50b70332652db22cdb3f1652074fb0307
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-06 01:26:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,16 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction ) }
   validates :title, exclusion: { in: %w("Guess" "Won't Believe") }
+  
+  validate :clickbaity_title
+
+  private
+
+  def clickbaity_title
+    if file_name.present? && !%w{.jpg .png .jpeg .tif}.include?(File.extname(file_name.downcase))
+      errors.add(:file_name, :invalid_extension)
+    end
+  end
+  
 
 end
